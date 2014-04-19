d4f5d3c4c7cb29108befec422571c1fecc1fc316
Marco Galletti <hello@marcogalletti.com>
2014-04-19 11:59:43 +0000

    Provisioning for project's vm

--- project-vm/provisioning.sh
+++ project-vm/provisioning.sh
@@ -3,18 +3,8 @@ apt-get install -y vim
 apt-get install -y git
 
 # Configure git data sci repo
-git config --global user.name "Marco Galletti"
-git config --global user.email "hello@marcogalletti.com"
-
-# https://help.github.com/articles/generating-ssh-keys
-# noLwcdIRnwLOp~fNJElR0K2WcYQvYe
-# http://stackoverflow.com/questions/17846529/could-not-open-a-connection-to-your-authentication-agent
-
-# Git Aliases
-# -------
-# ci = commit
-# st = status -sbu
-# co = checkout
-# br = branch
-# lg = log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
-# resethead = reset --hard HEAD
+date +%s | sha256sum | base64 | head -c 32 > /home/vagrant/.ssh/passphrase
+ssh-keygen -t rsa -C "hello@marcogalletti.com" -N `cat /home/vagrant/.ssh/passphrase` -f /home/vagrant/.ssh/id_rsa
+chown vagrant .ssh/id_rsa
+chown vagrant .ssh/id_rsa.pub
+eval `ssh-agent -s` #&& ssh-add ---> http://goo.gl/tiFJUN
